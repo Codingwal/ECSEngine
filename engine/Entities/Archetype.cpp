17b4b746116e7017dbff1c82d86b15349e8a1fba
Codingwal <florian.herzog08@gmail.com>
2024-12-03 12:23:07 +0100

    feature: implement Archetype::AddEntity

--- engine/Entities/Archetype.cpp
+++ engine/Entities/Archetype.cpp
@@ -13,14 +13,25 @@ Archetype::Archetype(ComponentType components[], int count)
         componentInfo.insert(std::pair(component.id, ComponentInfo(pos, component.size)));
         pos += component.size * ENTITIES_PER_CHUNK;
     }
-
-    chunks.push_back(ArchetypeChunk(pos));
-
     chunkSize = pos;
+
+    chunks.push_back(ArchetypeChunk(chunkSize));
 }
 
 void Archetype::AddEntity(Entity entity)
 {
+    int entityCount = entityToRow.size();
+    int chunkIndex = entityCount / ENTITIES_PER_CHUNK;
+    int indexInChunk = entityCount % ENTITIES_PER_CHUNK;
+
+    // Create a new chunk if necessary
+    if (chunkIndex >= chunks.size())
+    {
+        chunks.push_back(ArchetypeChunk(chunkSize));
+    }
+    ArchetypeChunk &chunk = chunks[chunkIndex];
+
+    entityToRow.insert(std::pair(entity, entityCount));
 }
 
 void Archetype::RemoveEntity(Entity entity)
@@ -29,5 +40,5 @@ void Archetype::RemoveEntity(Entity entity)
 
 std::string Archetype::ToString() const
 {
-    return "Archetype: {chunkSize: " + std::to_string(chunkSize) + ", entityCount: " + std::to_string(entityToRow.size()) + ", componentSet: " + componentSet.ToString() + "}";
+    return "Archetype: {chunkSize: " + std::to_string(chunkSize) + ", chunkCount: " + std::to_string(chunks.size()) + ", entityCount: " + std::to_string(entityToRow.size()) + ", componentSet: " + componentSet.ToString() + "}";
 }
