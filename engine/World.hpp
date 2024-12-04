58c2d0770dcc9208119db3960cb747360014a7db
Codingwal <florian.herzog08@gmail.com>
2024-12-04 18:46:53 +0100

    feature: generic entity creation method

--- engine/World.hpp
+++ engine/World.hpp
@@ -9,12 +9,16 @@ class World
 public:
     World();
 
+    template <typename... Ts>
+    Entity CreateEntity();
     Entity CreateEntity(ComponentType components[], int count);
     void DestroyEntity(Entity entity);
+
     template <typename T>
     void AddComponent(Entity entity);
     template <typename T>
     void RemoveComponent(Entity entity);
+
     template <typename T>
     void SetComponentData(Entity entity, T data);
     template <typename T>
@@ -23,7 +27,27 @@ public:
 public:
     EntityManager entityManager;
     ComponentManager componentManager;
+
+private:
+    template <typename T1, typename... Ts>
+    inline void CreateEntityHelper(ComponentType components[], size_t index);
 };
+
+template <typename T1, typename... Ts>
+inline void World::CreateEntityHelper(ComponentType components[], size_t index)
+{
+    components[index] = componentManager.GetOrRegisterComponent<T1>(); // Get and store the component id
+    if constexpr (sizeof...(Ts) > 0)                                   // Only continue the recursion if there are unhandled components left
+        CreateEntityHelper<Ts...>(components, --index);                // Recursion: call again for all components except the one handled in this iteration
+}
+template <typename... Ts>
+inline Entity World::CreateEntity()
+{
+    size_t componentCount = sizeof...(Ts);                     // Template arguments count
+    ComponentType components[componentCount];                  // Create an array with enough space for all components
+    CreateEntityHelper<Ts...>(components, componentCount - 1); // Start recursive array-initialization
+    return CreateEntity(components, componentCount);           // Create the entity with the now filled components array
+}
 
 template <typename T>
 inline void World::AddComponent(Entity entity)
--- main.cpp
+++ main.cpp
@@ -9,23 +9,17 @@ int main(int argc, char **argv)
 {
     World world = World();
 
-    ComponentType posType = world.componentManager.GetOrRegisterComponent<Position>();
-    ComponentType rotType = world.componentManager.GetOrRegisterComponent<Rotation>();
-    ComponentType sclType = world.componentManager.GetOrRegisterComponent<Scale>();
-
-    ComponentType posRotScl[] = {posType, rotType, sclType};
-    ComponentType rotScl[] = {rotType, sclType};
-
     Entity e;
-    e = world.CreateEntity(posRotScl, 3);
+    e = world.CreateEntity<Position, Rotation, Scale>();
     world.SetComponentData(e, Rotation(90, 0, 90));
     world.SetComponentData(e, Scale(2, 2, 2));
-    e = world.CreateEntity(posRotScl, 3);
+    e = world.CreateEntity<Position, Rotation, Scale>();
     world.SetComponentData(e, Rotation(180, 0, 90));
     world.SetComponentData(e, Position(10, 0, 10));
-    e = world.CreateEntity(rotScl, 2);
+    e = world.CreateEntity<Rotation, Scale>();
     world.SetComponentData(e, Scale(1, 1, 1));
 
+    ComponentType rotType = world.componentManager.GetOrRegisterComponent<Rotation>();
     EntityQuery rotQuery = EntityQuery(ComponentSet(&rotType, 1));
     for (Entity entity : rotQuery.GetEntities(world.entityManager))
     {
